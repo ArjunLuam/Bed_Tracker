cf510e4086465cc0591f7caff8c7e71e11c8feef
umang <iumangsharma001@gmail.com>
2020-08-16 01:51:26 +0530

    Hospital User Created

--- app/src/main/java/com/example/bedtracker/activity/BedInfo.java
+++ app/src/main/java/com/example/bedtracker/activity/BedInfo.java
@@ -5,12 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.bedtracker.R;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class BedInfo extends AppCompatActivity {
+
+    TextInputEditText mTotalBeds, mAvailableBeds;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bed_info);
+
+        mTotalBeds = findViewById(R.id.et_number);
+        mAvailableBeds = findViewById(R.id.et_model);
+
+
+
+
     }
 }
