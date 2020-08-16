9c81866f4d395391e2e194b44fa89c3b0c071212
umang <iumangsharma001@gmail.com>
2020-08-16 15:27:18 +0530

    Fragments Connected , UID working now!

--- app/src/main/java/com/example/bedtracker/activity/Home.java
+++ app/src/main/java/com/example/bedtracker/activity/Home.java
@@ -22,6 +22,6 @@ public class Home extends AppCompatActivity {
             window.setStatusBarColor(this.getResources().getColor(R.color.statusbar));
         }
 
-        //TODO : Umang will maintain the recycler view here
+        //TODO : Will maintain the recycler view here
     }
 }
